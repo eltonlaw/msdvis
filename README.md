f9435141a764ce78934ae83cf24df1b7d8b7171b
EltonLaw <eltonlaw296@gmail.com>
2016-11-20 23:28:17 -0500

    Plot artist location
    Cleaned up NaN values and duplicates then plotted data on basemap

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# million_songs_dataset
+# Million Song Dataset Data Exploration
 
-Dataset retrieved from: http://labrosa.ee.columbia.edu/millionsong/
-Helper Functions taken from: https://github.com/tbertinmahieux/MSongsDB/blob/master/PythonSrc/hdf5_getters.py
+* Dataset retrieved from: http://labrosa.ee.columbia.edu/millionsong/
+* Helper Functions taken from: https://github.com/tbertinmahieux/MSongsDB/blob/master/PythonSrc/hdf5_getters.py
 
+## Artist Location (sample set)
+
+![artist location](https://cloud.githubusercontent.com/assets/14999531/20470597/14aa9c6c-af78-11e6-8be5-fabc7a74490a.png)
--- model.py
+++ model.py
@@ -1,28 +1,56 @@
 import os
 import sys
 sys.path.append("./MSongsDB/PythonSrc")
-pd.set_option('display.expand_frame_repr', False)
 import hdf5_getters 
 import numpy as np
 import pandas as pd
+pd.set_option('display.expand_frame_repr', False)
 
 ### DATA ###
 h5_files = [x   for x in os.walk("./MillionSongSubset/data")  if len(x[0]) == 30 ] # There's the conditional to differentiate between different directory structures, anything less than 30 means the path is referencing a parent directory
 data = []
 
 for root,dirs,files in h5_files:
+	print "ROOT:",root
+	print "FILES:",files
 	for f in files:
+		print "Loading...",(files.index(f)),'/',len(files)
+		
 		h5file = hdf5_getters.open_h5_file_read(os.path.join(root,f))
-		mbtags=hdf5_getters.get_artist_mbtags(h5file)
-		similar_artists=hdf5_getters.get_similar_artists(h5file)
-		energy = hdf5_getters.get_energy(h5file)
-		artist_hottness = hdf5_getters.get_artist_hotttnesss(h5file)
-		h5file.close()
-		datapoint = {"mbtags":mbtags,"similar_artists":similar_artists,"energy":energy,"artist_hottness":artist_hottness}
-		data.append(datapoint)
+                a_id = hdf5_getters.get_artist_id(h5file)
+		a_latitude = hdf5_getters.get_artist_latitude(h5file)
+		a_longitude= hdf5_getters.get_artist_longitude(h5file)
+                h5file.close()
 
+                datapoint = {"id":a_id,"latitude":a_latitude,"longitude":a_longitude}
+                data.append(datapoint)
 df = pd.DataFrame(data)
+df = df.dropna(axis=0) # Delete rows with now values in lat/long
+df = df[df.duplicated()] # Delete duplicates
 
-print ""
-print df 
+lat_lon = df.as_matrix(columns=["latitude","longitude"])
+
+# Draw map
+print "drawing map..."
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+m = Basemap(projection='mill',
+        llcrnrlat =-90,
+        llcrnrlon=-180,
+        urcrnrlat=90,
+        urcrnrlon=180, 
+        resolution="c")
+m.drawcoastlines()
+m.drawcountries(linewidth=1)
+m.drawstates()
+m.bluemarble()
+# Plot artists
+print "plotting artist locations..."
+for artist_lat,artist_lon in lat_lon:
+    x_point,y_point = m(artist_lon,artist_lat)
+    m.plot(x_point,y_point,"*",markersize = 10)
+
+plt.title("Artist Locations")
+plt.savefig("artist_location.png",bbox_inches="tight")
+
 
