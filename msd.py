989fda595792999645a6517cb3a09ced64e82662
EltonLaw <eltonlaw296@gmail.com>
2017-01-11 19:50:31 -0500

    Fixed start/end indexing parameter

--- msd.py
+++ msd.py
@@ -4,7 +4,7 @@ import pandas as pd
 sys.path.append("./MSongsDB/PythonSrc")
 import hdf5_getters
 
-def get_data(categories,path="./MillionSongSubset/data",write=False,write_path="./temp/data.csv",num_songs=10000):
+def get_data(categories,path="./MillionSongSubset/data",write=False,write_path="./temp/data.csv",start_i=0,end_i=10000):
     """ Pulls data from all h5 files on the provided path and all it's child nodes 
 
     Parameters 
@@ -24,20 +24,24 @@ def get_data(categories,path="./MillionSongSubset/data",write=False,write_path="
 
     """
     h5_files = [x   for x in os.walk(path)  if len(x[0]) == 30 ] # There's the conditional to differentiate between different directory structures, anything less than 30 means the path is referencing a parent directory
-    data,count = [],0
-    for root,dirs,files in h5_files[:num_songs]:
+    data,file_paths= [],[]
+    count = 0
+    for root,dirs,files in h5_files:
         for f in files:
-            h5file = hdf5_getters.open_h5_file_read(os.path.join(root,f))
-            datapoint = {}
-            for cat in categories: 
-                datapoint[cat] = getattr(hdf5_getters,"get_"+cat)(h5file)
-            h5file.close()
-            data.append(datapoint)
+            file_paths.append(os.path.join(root,f)) 
 
-            progress = (count*100.)/10000 # 10 000 datapoints in sample 
-            sys.stdout.write("==== Compiling subset of data... [ {:.2f}% ] ==== \r".format(progress))
-            sys.stdout.flush()
-            count +=1
+    for file_path in file_paths[start_i:end_i]:
+        h5file = hdf5_getters.open_h5_file_read(file_path)
+        datapoint = {}
+        for cat in categories: 
+            datapoint[cat] = getattr(hdf5_getters,"get_"+cat)(h5file)
+        h5file.close()
+        data.append(datapoint)
+
+        progress = (count*100.)/10000 # 10 000 datapoints in sample 
+        sys.stdout.write("==== Compiling subset of data... [ {:.2f}% ] ==== \r".format(progress))
+        sys.stdout.flush()
+        count +=1
     df = pd.DataFrame(data)
     if write:
         df.to_csv(write_path)
