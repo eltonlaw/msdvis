5c6c7ed3596065b2a14875d01f88d55ad63f25ef
EltonLaw <eltonlaw296@gmail.com>
2017-01-13 17:54:31 -0500

    Added function to grab song meta data

--- msd.py
+++ msd.py
@@ -1,16 +1,18 @@
 import os
+import numpy as np
 import sys
 import pandas as pd
+import sqlite3
 sys.path.append("./MSongsDB/PythonSrc")
 import hdf5_getters
 
-def get_data(categories,path="./MillionSongSubset/data",write=False,write_path="./temp/data.csv",start_i=0,end_i=10000):
+def get_song_data(categories,path="./MillionSongSubset/data",write=False,write_path="./temp/song_data.csv",start_i=0,end_i=10000):
     """ Pulls data from all h5 files on the provided path and all it's child nodes 
 
     Parameters 
     ---------
     categories: List/Strings
-        Strings are specific keywords. For a full list of available categories and more information: http://labrosa.ee.columbia.edu/millionsong/pages/example-track-description
+        Strings are specific keywords. For an example of available categories: http://labrosa.ee.columbia.edu/millionsong/pages/example-track-description
     path: String,optional
         Path to the h5 data to decode and compile
     write: Boolean,optional
@@ -47,3 +49,47 @@ def get_data(categories,path="./MillionSongSubset/data",write=False,write_path="
         df.to_csv(write_path)
         print "Subset of data written to"+write_path
     return df
+
+def get_meta_data(files=None,path="./MillionSongSubset/AdditionalFiles",write=False,write_path="./temp/meta_data.csv"):
+    """ Dataset provides some summary data in the form of '.txt', '.h5' and '.db' files to help navigate the data 
+    
+    Parameters
+    ----------
+    categories: List
+        Valid categories: ['subset_artist_location.txt', 'subset_artist_similarity.db', 'subset_artist_term.db', 'subset_msd_summary_file.h5', 'subset_track_metadata.db', 'subset_tracks_per_year.txt', 'subset_unique_artists.txt', 'subset_unique_mbtags.txt', 'subset_unique_terms.txt', 'subset_unique_tracks.txt']
+
+    Returns
+    ------
+    Vector, each element represents one category
+    
+    """ 
+    # If no specific files are selected, get them all
+    if files == None:
+        root,dirs,files = [x for x in os.walk(path)][0] 
+        ignore = ["LICENSE","README"]
+        for ii in ignore:
+            files.remove(ii)
+    
+    data = []
+    for f in files:
+        file_path = os.path.join(path,f)
+        if f.split(".")[1] == "txt":
+            datapoint = np.genfromtxt(file_path,delimiter="<SEP>")
+            data.append(datapoint)
+        elif f.split(".")[1] == "db":
+            file_path = os.path.join(path,"subset_artist_similarity.db")
+            conn = sqlite3.connect(file_path)
+            c = conn.cursor()
+            c.execute("SELECT name FROM sqlite_master WHERE type='table'")
+            tables = c.fetchall()
+            for table_name in tables:
+                c.execute("SELECT * FROM " + str(table_name[0]))
+                datapoint = c.fetchall()
+                data.append(datapoint)
+            conn.close()
+        elif f.split(".")[1] == "h5":
+            file_path = os.path.join(path,"subset_msd_summary_file.h5")
+            h5file = hdf5_getters.open_h5_file_read(file_path)
+            data.append(h5file) 
+            h5file.close()
+    return data
