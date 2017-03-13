5dc85f993905e6944ae4061e407c1519bb4d7e5d
EltonLaw <eltonlaw296@gmail.com>
2017-03-13 03:41:47 -0400

    add run.py

---
+++ run.py
@@ -0,0 +1,16 @@
+from model import basic_info
+from model import freq_plot
+from model import world_plot
+from model import stacked_barplot
+from model import compare_to_average
+from model import error_bar
+from model import dr
+
+if __name__ == "__main__":
+    basic_info()
+    freq_plot()
+    world_plot()
+    stacked_barplot()
+    compare_to_average()
+    error_bar()
+    dr()
