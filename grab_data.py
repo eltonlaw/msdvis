d0f3c4511dc84e07f306ab219348ccd6fc3933df
EltonLaw <eltonlaw296@gmail.com>
2017-04-17 17:44:34 -0400

    add multiprocessing

--- grab_data.py
+++ grab_data.py
@@ -3,83 +3,93 @@ import numpy as np
 import sys
 import pandas as pd
 import sqlite3
+import multiprocessing
 sys.path.append("./MSongsDB/PythonSrc")
 import hdf5_getters
-import multiprocessing
 
 
-def get_song_data(categories,path="./MillionSongSubset/data",write=False,write_path="./temp/song_data.csv",start_i=0,end_i=10000):
-    """ Pulls data from all h5 files on the provided path and all it's child nodes 
+def get_song_data(categories, path="./MillionSongSubset/data", write=False,
+                  write_path="./temp/song_data.csv", start_i=0, end_i=10000):
+    """ Pulls data from all h5 files on the provided path and all it's child nodes
 
-    Parameters 
+    Parameters
     ---------
     categories: List/Strings
-        Strings are specific keywords. For an example of available categories: http://labrosa.ee.columbia.edu/millionsong/pages/example-track-description
+        Strings are specific keywords. For an example of available categories:
+        http://labrosa.ee.columbia.edu/millionsong/pages/example-track-description
     path: String,optional
         Path to the h5 data to decode and compile
     write: Boolean,optional
-         If true, writes the final compiled pandas Dataframe into a csv file located at "./temp/data.csv"
+        If true, writes data to .csv at 'write_path'
     write_path: String,optional
-        If write = True, it will be the destination of the final compiled pandas Dataframe.
-        
+        Destination of the final compiled pandas Dataframe.
+
     Returns
     ------
-    Pandas Dataframe containing categories 
+    Pandas Dataframe containing categories
 
     """
-    h5_files = [x   for x in os.walk(path)  if len(x[0]) == 30 ] # There's the conditional to differentiate between different directory structures, anything less than 30 means the path is referencing a parent directory
-    data,file_paths= [],[]
+# Conditional differentiates between different directory structures,
+# anything less than 30 means the path is referencing a parent directory
+    h5_files = [x for x in os.walk(path) if len(x[0]) == 30]
+    data, file_paths = [], []
     count = 0
-    for root,dirs,files in h5_files:
+    for root, dirs, files in h5_files:
         for f in files:
-            file_paths.append(os.path.join(root,f)) 
+            file_paths.append(os.path.join(root, f))
 
     for file_path in file_paths[start_i:end_i]:
         h5file = hdf5_getters.open_h5_file_read(file_path)
         datapoint = {}
-        for cat in categories: 
-            datapoint[cat] = getattr(hdf5_getters,"get_"+cat)(h5file)
+        for cat in categories:
+            datapoint[cat] = getattr(hdf5_getters, "get_"+cat)(h5file)
         h5file.close()
         data.append(datapoint)
 
-        progress = (count*100.)/10000 # 10 000 datapoints in sample 
-        sys.stdout.write("==== Compiling subset of data... [ {:.2f}% ] ==== \r".format(progress))
+        progress = (count*100.)/10000  # 10 000 datapoints in sample
+        sys.stdout.write("==== Compiling subset of data...[ {:.2f}% ] ==== \r".format(progress))
         sys.stdout.flush()
-        count +=1
+        count += 1
     df = pd.DataFrame(data)
     if write:
         df.to_csv(write_path)
-        print "Subset of data written to"+write_path
+        print("Data written to {0}".format(write_path))
     return df
 
-def get_meta_data(files=None,path="./MillionSongSubset/AdditionalFiles",write=False,write_path="./temp/meta_data.csv"):
-    """ Dataset provides some summary data in the form of '.txt', '.h5' and '.db' files to help navigate the data 
-    
+
+def get_meta_data(files=None, path="./MillionSongSubset/AdditionalFiles",
+                  write=False, write_path="./temp/meta_data.csv"):
+    """ Dataset provides some summary data in the form of '.txt', '.h5' and
+    '.db' files to help navigate the data
+
     Parameters
     ----------
     categories: List
-        Valid categories: ['subset_artist_location.txt', 'subset_artist_similarity.db', 'subset_artist_term.db', 'subset_msd_summary_file.h5', 'subset_track_metadata.db', 'subset_tracks_per_year.txt', 'subset_unique_artists.txt', 'subset_unique_mbtags.txt', 'subset_unique_terms.txt', 'subset_unique_tracks.txt']
+        Valid categories: ['subset_artist_location.txt',
+        'subset_artist_similarity.db', 'subset_artist_term.db',
+        'subset_msd_summary_file.h5', 'subset_track_metadata.db',
+        'subset_tracks_per_year.txt', 'subset_unique_artists.txt',
+        'subset_unique_mbtags.txt', 'subset_unique_terms.txt',
+        'subset_unique_tracks.txt']
 
     Returns
     ------
     Vector, each element represents one category
-    
-    """ 
+    """
     # If no specific files are selected, get them all
-    if files == None:
-        root,dirs,files = [x for x in os.walk(path)][0] 
-        ignore = ["LICENSE","README"]
+    if files is None:
+        root, dirs, files = [x for x in os.walk(path)][0]
+        ignore = ["LICENSE", "README"]
         for ii in ignore:
             files.remove(ii)
-    
     data = []
     for f in files:
-        file_path = os.path.join(path,f)
+        file_path = os.path.join(path, f)
         if f.split(".")[1] == "txt":
-            datapoint = np.genfromtxt(file_path,delimiter="<SEP>")
+            datapoint = np.genfromtxt(file_path, delimiter="<SEP>")
             data.append(datapoint)
         elif f.split(".")[1] == "db":
-            file_path = os.path.join(path,"subset_artist_similarity.db")
+            file_path = os.path.join(path, "subset_artist_similarity.db")
             conn = sqlite3.connect(file_path)
             c = conn.cursor()
             c.execute("SELECT name FROM sqlite_master WHERE type='table'")
@@ -90,7 +100,14 @@ def get_meta_data(files=None,path="./MillionSongSubset/AdditionalFiles",write=Fa
                 data.append(datapoint)
             conn.close()
         elif f.split(".")[1] == "h5":
-            file_path = os.path.join(path,"subset_msd_summary_file.h5")
+            file_path = os.path.join(path, "subset_msd_summary_file.h5")
             h5file = hdf5_getters.open_h5_file_read(file_path)
-            data.append(h5file) 
+            data.append(h5file)
             h5file.close()
+
+
+if __name__ == "__main__":
+    print("...running grab_data.py")
+    CPU_COUNT = multiprocessing.cpu_count()
+    p = multiprocessing.Pool(processes=CPU_COUNT)
+    p.apply_async(get_song_data(["year"], write=True))
